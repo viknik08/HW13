d128fffa0a8afa29c106197f5c700cf674a2a3fa
viknik08 <63363344+viknik08@users.noreply.github.com>
2022-08-25 16:08:24 +0800

    delete storyboard + create group

---
+++ HW13/Sources/Controllers/SettingsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsViewController.swift
+//  HW13
+//
+//  Created by Виктор Басиев on 25.08.2022.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+
+
+}
+
